b595743d13c08f8dc35a71f645b4509dfd6358a5
ballance <ballance@gmail.com>
2011-12-14 14:08:44 -0700

    Pushing new changes

--- source/app.specs/ViewTheDepartmentsInADepartmentSpecs.cs
+++ source/app.specs/ViewTheDepartmentsInADepartmentSpecs.cs
@@ -1,4 +1,5 @@
-﻿ using Machine.Specifications;
+﻿ using System.Collections.Generic;
+ using Machine.Specifications;
  using app.web.application.catalogbrowsing;
  using app.web.core;
  using developwithpassion.specifications.rhinomocks;
@@ -18,8 +19,29 @@ namespace app.specs
    
     public class when_run : concern
     {
-      It it_should_show_departments_in_a_main_department = () =>        
+        private Establish c = () =>
+        {
+            department_repository = depends.on<IFindDepartments>();
+            display_engine = depends.on<IDisplayReports>();
+            request = fake.an<IProvideDetailsToCommands>();
+            the_main_departments = new DepartmentItem();
+            the_sub_departments = new List<DepartmentItem> {new DepartmentItem()};
+
+
+
+
+        };
+
+      It it_should_show_departments_in_a_main_department = () =>
+        display_engine.received(x => x.display(the_main_departments));
+
+      static IFindDepartments department_repository;
+      static IProvideDetailsToCommands request;
+      static IDisplayReports  display_engine;
+      static IEnumerable<DepartmentItem> the_main_departments;
+
         
+        static IEnumerable<DepartmentItem> the_sub_departments;
     }
   }
 }
