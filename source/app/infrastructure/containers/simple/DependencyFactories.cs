a520cc56f495c309d6c84b0c482c180e165ee1ab
ballance <ballance@gmail.com>
2011-12-15 14:52:29 -0700

    Pushing new changes

--- source/app/infrastructure/containers/simple/DependencyFactories.cs
+++ source/app/infrastructure/containers/simple/DependencyFactories.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace app.infrastructure.containers.simple
 {
   public class DependencyFactories : IFindFactoriesThatCanCreateDependencies
   {
-    public ICreateASingleDependency get_factory_that_can_create(Type dependency)
-    {
-      throw new NotImplementedException();
-    }
+      private IEnumerable<ICreateASingleDependency> dependencies;
+
+      public DependencyFactories(IEnumerable<ICreateASingleDependency> dependencies)
+      {
+          this.dependencies = dependencies;
+      }
+
+      public ICreateASingleDependency get_factory_that_can_create(Type dependency)
+      {
+          ICreateASingleDependency toReturn = null; 
+          foreach(var eachDependency in this.dependencies)
+          {
+              if (eachDependency.can_create(dependency))
+                  toReturn =  eachDependency;
+          }
+          return toReturn;
+      }
   }
 }
