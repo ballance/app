e21796d6571d4cb907963cc76e0773446ab71308
ballance <ballance@gmail.com>
2011-12-15 16:02:24 -0700

    Pushing new changes

--- source/app/infrastructure/containers/simple/SingleDependencyFactory.cs
+++ source/app/infrastructure/containers/simple/SingleDependencyFactory.cs
@@ -5,17 +5,18 @@ namespace app.infrastructure.containers.simple
 	public class SingleDependencyFactory : ICreateASingleDependency
 	{
 		private IMatchAType type_criteria;
+	    private ICreateADependency _aDependency;
 
-		public SingleDependencyFactory(IMatchAType type_criteria)
-		{
-			this.type_criteria = type_criteria;
+	    public SingleDependencyFactory(IMatchAType type_criteria, ICreateADependency aDependency)
+	    {
+	        _aDependency = aDependency;
+	        this.type_criteria = type_criteria;
+	    }
 
-		}
-
-		public object create()
-		{
-			throw new NotImplementedException();
-		}
+	    public object create()
+	    {
+	        return _aDependency.create();
+	    }
 
 		public bool can_create(Type type)
 		{
